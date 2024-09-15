eca920611be0d9a6761930492ef2714f5b099636
Maciej Kopeć <maciejkopec92@gmail.com>
2024-09-15 15:38:01 +0200

    feat: improve code block

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,32 +1,38 @@
-import React from 'react';
-import type { MDXComponents } from 'mdx/types'
-import Image from 'next/image';
 import { Button } from "@/components/ui/button";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneLight as codeStyle } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    img: ({ src, alt }) => <Image src={src as string} alt={alt as string} className="mb-4" />,
-    Button: ({ children, href }) => <Button asChild><a href={href}>{children}</a></Button>,
+    img: ({ src, alt }) => (
+      <Image src={src as string} alt={alt as string} className="mb-4" />
+    ),
+    Button: ({ children, href }) => (
+      <Button asChild>
+        <a href={href}>{children}</a>
+      </Button>
+    ),
     pre: ({ children }) => {
       const childrenArray = React.Children.toArray(children);
       const code = childrenArray[0] as React.ReactElement;
-      const className = code.props.className || '';
-      const language = className.replace(/language-/, '');
+      const className = code.props.className || "";
+      const language = className.replace(/language-/, "");
 
       return (
         <div className="relative">
-          <div className="absolute top-0 right-0 bg-gray-700 text-gray-200 px-2 py-1 text-xs rounded-tr">
+          <div className="bg-zinc-200 text-gray-700 px-4 py-2 text-sm rounded-t-lg">
             {language}
           </div>
           <SyntaxHighlighter
             language={language}
-            style={tomorrow}
+            style={codeStyle}
             customStyle={{
               margin: 0,
-              borderRadius: '0.375rem',
-              padding: '1rem',
+              borderRadius: "0 0 0.375rem 0.375rem",
+              padding: "1rem",
             }}
           >
             {code.props.children.trim()}
@@ -35,5 +41,5 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       );
     },
     ...components,
-  }
+  };
 }
