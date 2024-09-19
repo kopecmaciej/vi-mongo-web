a39c7ed22c6df689f89454658e860332ea0c50e5
Maciej Kopeć <maciejkopec92@gmail.com>
2024-09-19 18:26:42 +0200

    feat: improve installation page

---
+++ src/components/Docs/Installation.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import { useState } from 'react';
+import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from '@/components/ui/select';
+import { Pre } from '@/mdx-components';
+
+const InstallationPage = () => {
+  const [os, setOs] = useState<string>('Linux');
+  const [arch, setArch] = useState<string>('x86_64');
+
+  const handleOsChange = (value: string) => {
+    setOs(value);
+    setArch('x86_64'); // Reset architecture to default when OS changes
+  };
+
+  const handleArchChange = (value: string) => {
+    setArch(value);
+  };
+
+  const getDownloadFileName = () => {
+    if (!os || !arch) return '';
+    return `vi-mongo_${os}_${arch}.${os === 'Windows' ? 'zip' : 'tar.gz'}`;
+  };
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Installation</h1>
+      <h2 className="text-xl mb-4">Select your OS and Processor</h2>
+      <div className="mb-4">
+        <label className="block mb-2">OS:</label>
+        <Select value={os} onValueChange={handleOsChange}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select OS" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Darwin">macOS</SelectItem>
+            <SelectItem value="Linux">Linux</SelectItem>
+            <SelectItem value="Windows">Windows</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      {os && (
+        <div className="mb-4">
+          <label className="block mb-2">Architecture:</label>
+          <Select value={arch} onValueChange={handleArchChange}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Architecture" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="arm64">ARM64</SelectItem>
+              <SelectItem value="x86_64">x86_64</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      )}
+      {os && arch && (
+        <div>
+          <h3 className="text-lg font-semibold mb-2">Download and Install</h3>
+          {os === 'Linux' && (
+            <>
+              <h4 className="font-semibold">Using curl</h4>
+              <Pre>
+                <code className="language-bash">
+                  {`curl -s https://api.github.com/repos/kopecmaciej/vi-mongo/releases/latest | grep "browser_download_url.*${getDownloadFileName()}" | cut -d '"' -f 4 | xargs curl -LO
+tar -xzf ${getDownloadFileName()}
+chmod +x vimongo
+sudo mv vimongo /usr/bin`}
+                </code>
+              </Pre>
+              <h4 className="font-semibold">Using wget</h4>
+              <Pre>
+                <code className="language-bash">
+                  {`wget -qO- https://api.github.com/repos/kopecmaciej/vi-mongo/releases/latest | grep "browser_download_url.*${getDownloadFileName()}" | cut -d '"' -f 4 | xargs wget -q --show-progress &&
+tar -xzf ${getDownloadFileName()} &&
+chmod +x vi-mongo &&
+sudo mv vi-mongo /usr/bin`}
+                </code>
+              </Pre>
+            </>
+          )}
+          {os === 'Darwin' && (
+            <Pre>
+              <code className="language-bash">
+                {`curl -s https://api.github.com/repos/kopecmaciej/vi-mongo/releases/latest | grep "browser_download_url.*${getDownloadFileName()}" | cut -d '"' -f 4 | xargs curl -LO
+tar -xzf ${getDownloadFileName()}
+chmod +x vimongo
+sudo mv vimongo /usr/bin`}
+              </code>
+            </Pre>
+          )}
+          {os === 'Windows' && (
+            <Pre>
+              <code className="language-powershell">
+                {`Invoke-WebRequest -Uri "https://api.github.com/repos/kopecmaciej/vi-mongo/releases/latest" -OutFile "vi-mongo.zip"
+Expand-Archive -Path "vi-mongo.zip" -DestinationPath "."
+Move-Item -Path "vimongo.exe" -Destination "C:\\Program Files\\vimongo"`}
+              </code>
+            </Pre>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InstallationPage;
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,61 @@
 import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipProvider } from "@/components/ui/tooltip";
+import { Copy } from "lucide-react";
 import type { MDXComponents } from "mdx/types";
 import Image from "next/image";
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneLight as codeStyle } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+export const Pre = ({ children }: { children: React.ReactNode }) => {
+  const childrenArray = React.Children.toArray(children);
+  const code = childrenArray[0] as React.ReactElement;
+  const className = code.props.className || "";
+  const language = className.replace(/language-/, "");
+  const codeText = code.props.children.trim();
+  const [copied, setCopied] = React.useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(codeText).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+    });
+  };
+
+  return (
+    <TooltipProvider>
+      <div className="relative">
+        <div className="bg-zinc-200 text-gray-700 px-4 py-2 text-sm rounded-t-lg flex justify-between items-center">
+          <span>{language}</span>
+          <Tooltip>
+            <div className="flex items-center">
+              {copied && (
+                <span className="text-xs text-blue-500 mr-2">Copied!</span>
+              )}
+              <Button
+                onClick={handleCopy}
+                className="ml-2 bg-transparent text-gray-500 hover:text-gray-700"
+              >
+                <Copy size={16} />
+              </Button>
+            </div>
+          </Tooltip>
+        </div>
+        <SyntaxHighlighter
+          language={language}
+          style={codeStyle}
+          customStyle={{
+            margin: 0,
+            borderRadius: "0 0 0.375rem 0.375rem",
+            padding: "1rem",
+          }}
+        >
+          {codeText}
+        </SyntaxHighlighter>
+      </div>
+    </TooltipProvider>
+  );
+};
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -15,52 +67,28 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         <a href={href}>{children}</a>
       </Button>
     ),
-    pre: ({ children }) => {
-      const childrenArray = React.Children.toArray(children);
-      const code = childrenArray[0] as React.ReactElement;
-      const className = code.props.className || "";
-      const language = className.replace(/language-/, "");
-
-      return (
-        <div className="relative">
-          <div className="bg-zinc-200 text-gray-700 px-4 py-2 text-sm rounded-t-lg">
-            {language}
-          </div>
-          <SyntaxHighlighter
-            language={language}
-            style={codeStyle}
-            customStyle={{
-              margin: 0,
-              borderRadius: "0 0 0.375rem 0.375rem",
-              padding: "1rem",
-            }}
-          >
-            {code.props.children.trim()}
-          </SyntaxHighlighter>
-        </div>
-      );
-    },
-    code: ({ children }) => (
-      <code className="bg-gray-100 text-[#047097] px-1 py-0.5 rounded">
+    pre: ({ children, ...props }) => <Pre {...props}>{children}</Pre>,
+    code: ({ children, ...props }) => (
+      <code
+        className="bg-gray-100 text-[#047097] px-1 py-0.5 rounded"
+        {...props}
+      >
         {children}
       </code>
     ),
-    hr: () => (
-      <hr className="mb-6 h-px border-0 bg-pink-100" />
-    ),
+    hr: () => <hr className="mb-6 h-px border-0 bg-pink-100" />,
     ul: ({ children }) => (
       <ul className="list-disc pl-6 space-y-2 text-gray-700">{children}</ul>
     ),
-    li: ({ children }) => (
-      <li className="text-base">{children}</li>
-    ),
+    li: ({ children }) => <li className="text-base">{children}</li>,
     h1: ({ children }) => (
       <h1 className="text-3xl font-bold text-gray-800">{children}</h1>
     ),
     h2: ({ children }) => (
-      <h2 className="text-2xl font-semibold m-0 mb-2 text-gray-800">{children}</h2>
+      <h2 className="text-2xl font-semibold m-0 mb-2 text-gray-800">
+        {children}
+      </h2>
     ),
     ...components,
   };
 }
-
